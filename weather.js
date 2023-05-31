1f5421d42d3efc7bd60b4efc01f304977c62c953
Jordan Pufon <fabovepufonjordan@gmail.com>
2023-05-31 14:09:02 -0500

    first commit

---
+++ weather.js
@@ -0,0 +1,39 @@
+const apikey = "edc88ac70761d79e9b684659a636f123"
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+
+async function checkWeather(city){
+    const response = await fetch(apiurl+city+ `&appid=${apikey}`);
+    let data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".Wind").innerHTML = data.main.wind.speed + "km/h";
+    if(data.weather[0].main == " Clouds"){
+        weatherIcon.src = "images/clouds.png";
+        
+    }else if(data.weather[0].main ===" Rain"){
+        weatherIcon.src = "images/rain.png";
+    }else if(data.weather[0].main == " Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    
+    
+    }
+    else if(data.weather[0].main == " Clear"){
+        weatherIcon.src = "images/clear.png";
+    
+    
+    }
+
+    }
+
+searchBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    checkWeather(searchBox.value); 
+} )
+
